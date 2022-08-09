48bef0150a1c2797b048f1e4c98108da6b683421
dudematthew <mmoczydlowski4@gmail.com>
2022-08-09 12:42:18 +0200

    Change project name

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,14 @@
 # abort on errors
 # set -e
 
-# build
-npm run build > deploy.log
+# merge branch
+git checkout production
 
-cp ./404.html ./dist/
+git merge main
 
-# navigate into the build output directory
-cd dist
+git push
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init > deploy.log
-git add -A > deploy.log
-git commit -m 'deploy' > deploy.log
-git remote add origin https://github.com/dudematthew/boost-rpg-tools.git > deploy.log
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f origin HEAD:gh-pages > deploy.log
-
-cd -
+git checkout main
 
 # if you want to keep the terminal open
-/bin/bash
+# /bin/bash

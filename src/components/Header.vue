cc713c713dff2256842267beb53ac3946c94f976
dudematthew <mmoczydlowski4@gmail.com>
2022-08-11 23:55:23 +0200

    Improve Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,37 +18,35 @@
 
       <ul class="right hide-on-med-and-down">
         <li><a href="https://patronite.pl/obozy" target="_blank"><span class="valign-wrapper black-text"><i
-                class="grey-text material-icons">local_fire_department</i>&nbsp; Zostań naszym Patronem</span></a></li>
+                class="green-text material-icons text-darken-2">local_fire_department</i>&nbsp; Zostań naszym Patronem</span></a></li>
       </ul>
 
       <ul class="right hide-on-med-and-down">
         <li><a href="https://www.instagram.com/obozy_gra_terenowa" target="_blank"><span
-              class="valign-wrapper black-text"><i class="grey-text material-icons">center_focus_strong</i>&nbsp; Nasz
+              class="valign-wrapper black-text"><i class="green-text material-icons text-darken-2">center_focus_strong</i>&nbsp; Nasz
               Instagram</span></a></li>
       </ul>
 
       <ul class="right hide-on-med-and-down">
         <li><a href="https://www.facebook.com/ObozyGraTerenowa" target="_blank"><span
-              class="valign-wrapper black-text"><i class="grey-text material-icons">public</i>&nbsp; Nasz
+              class="valign-wrapper black-text"><i class="green-text material-icons text-darken-2">public</i>&nbsp; Nasz
               Facebook</span></a></li>
       </ul>
 
       <ul class="right hide-on-med-and-down">
         <li>
-          <router-link to="/dolacz-do-nas"><span class="valign-wrapper black-text"><i
-                class="grey-text material-icons">hand_gesture</i>&nbsp; Dołącz do nas</span></router-link>
+          <router-link to="/dolacz-do-nas"><span class="valign-wrapper black-text"><i class="green-text material-icons text-darken-2">group_add</i>&nbsp; Dołącz do nas</span></router-link>
         </li>
       </ul>
 
           <ul class="right hide-on-med-and-down">
             <li>
-              <router-link to="/o-nas"><span class="valign-wrapper black-text"><i
-                    class="grey-text material-icons">lightbulb_circle</i>&nbsp; O Obozach</span></router-link>
+              <router-link to="/o-nas"><span class="valign-wrapper black-text"><i class="green-text material-icons text-darken-2">lightbulb_circle</i>&nbsp; O Obozach</span></router-link>
             </li>
           </ul>
 
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">O Obozach</a></li>
+        <li><router-link to="/o-nas">O Obozach</router-link></li>
         <li><a href="#">Dowiedz się więcej</a></li>
         <li><a href="https://www.facebook.com/ObozyGraTerenowa" target="_blank">Nasz Facebook</a></li>
         <li><a href="https://www.instagram.com/obozy_gra_terenowa" target="_blank">Nasz Instagram</a></li>
